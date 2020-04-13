eb00c08e399f15273bd510765a2b5845758881a6
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-04-13 04:29:20 -0300

    chore: Configure Nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,4 +6,6 @@ app.get('/', (request, response) => {
   return response.json({ message: 'Hello World Rocketseat'});
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Back-end started!');
+});
